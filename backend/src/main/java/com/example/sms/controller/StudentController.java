debf9763598c8799bab92600aedbd7011d412656
localrepo <ankitmt17@gmail.com>
2026-09-08 15:21:45 +0530

    Day 4 Stream API and Lambda

--- backend/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/src/main/java/com/example/sms/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.example.sms.controller;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +18,29 @@ public class StudentController {
 
     @GetMapping
     public ArrayList<Student> getStudent() {
-     ArrayList<Student> students = new ArrayList<>();
+    ArrayList<Student> students = new ArrayList<>();
       
-     students.add(new Student(1, "Ankit", "B.E"));
-     students.add(new Student(2, "Rahul", "MCA"));
+    students.add(new Student(1, "John", "Mca"));
+    students.add(new Student(2, "Tushar", "Bca"));
+    students.add(new Student(3, "Aman", "Bca"));
+    students.add(new Student(2, "Rahul", "Mca"));
+    students.add(new Student(3, "Ajay", "Mca"));
 
-     return students;
+    return students;
 }
+@GetMapping("/bca")
+public List<Student> getBcaStudent(){
+    return getStudent().stream()
+                    .filter((Student -> "Bca".equals(Student.getCourse())))
+                    .collect(Collectors.toList());
+    
+}
+@GetMapping("/name")
+public List<String> getName(){
+    return getStudent().stream()
+                       .map(Student::getName)
+                       .collect(Collectors.toList());
+}
+
+
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,94 @@
-
 import { useEffect, useState } from "react";
 
-function App(){
-  /*
-  const[student, setStudent] = useState(null);
+function App() {
 
-  const getStudent = async() => {
+  // For single student
+  const [student, setStudent] = useState(null);
+
+  // For all students
+  const [students, setStudents] = useState([]);
+
+  // For Bca student
+  const [bcaStudents, setBcaStudents] = useState([]);
+
+  // Get single student
+  const getSingleStudent = async () => {
+
     const response = await fetch("http://localhost:8080/student");
+
     const data = await response.json();
+
     setStudent(data);
-  
   };
+
+  // Get all students
+  const getStudents = async () => {
+
+    const response = await fetch("http://localhost:8080/student");
+
+    const data = await response.json();
+
+    setStudents(data);
+  };
+  //Get Bca students
+  const getBcaStudents = async () => {
+    const response = await fetch("http://localhost:8080/student/bca");
+    const data = await response.json();
+    setBcaStudents(data);
+  }
+
+  // Automatically get all students when page loads
+  useEffect(() => {
+    getSingleStudent;
+    getStudents();
+    getBcaStudents();
+  }, []);
+
   return (
-    <div style = {{padding : "40px" }}>
-      <button onClick = {getStudent}>
+    <div style={{ padding: "40px" }}>
+
+      {/* Single Student */}
+      <h1>Get Single Student</h1>
+
+      <button onClick={getSingleStudent}>
         Get Student
       </button>
-      {
-        student && (
-          <div>
-            <h2>ID : {student.id}</h2>
-            <h2>Name : {student.name}</h2>
-            <h2>Course : {student.course}</h2>
-          </div>
-        )
-      }
-    </div>
-  ); */
-  const[students, setStudents] = useState([]);
 
-  const getStudent = async() => {
-    const response = await fetch("http://localhost:8080/student");
+      {student && (
+        <div>
+          <h2>ID : {student.id}</h2>
+          <h2>Name : {student.name}</h2>
+          <h2>Course : {student.course}</h2>
+        </div>
+      )}
 
-    const data = await response.json();
-    setStudents(data);
-  }; 
 
-  useEffect( () => {
-    getStudent();
-  }, []);  
-
-  return (
-    <div>
-
+      {/* Student List */}
       <h1>Student List</h1>
 
-      {
-        students.map((student) => (
-          <div key={student.id}>
-          
-            <h3> 
-            {student.name} - {student.course}  
+      {students.map((student) => (
+        <div key={student.id}>
+          <h3>
+            {student.name} - {student.course}
+          </h3>
+        </div>
+      ))}
 
-            </h3>
-          </div>
-        ))
-      }
+       {/* BCA STUDENTS */}
+
+       <h1>BCA Students</h1>
+
+{bcaStudents.map(student => (
+  <div key={student.id}>
+    <h3>
+      {student.name} - {student.course}
+    </h3>
+  </div>
+))}
+
 
     </div>
   );
 }
+
 export default App;
